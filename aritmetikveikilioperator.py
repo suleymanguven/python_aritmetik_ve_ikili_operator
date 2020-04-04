02fe70d18165e2dba6c40008a0cc2234b49d454e
suleymanguven <34272909+suleymanguven@users.noreply.github.com>
2020-04-05 00:40:03 +0300

    Add files via upload

---
+++ aritmetikveikilioperator.py
@@ -0,0 +1,23 @@
+x=10
+y=3
+
+toplam=x+y
+fark=x-y
+carpim=x*y
+bolme=(x/y)
+bolum=x//y
+kalan=x%y
+kuvvet=x**2
+print("Toplam: ",toplam)
+
+print("Fark: ",fark)
+print("Çarpım: ",carpim)
+print("Bölme: ",round(bolme,2))
+print("Bölüm: ",bolum)
+print("Kalan: ",kalan)
+print("x'in Y. kuvveti",kuvvet)
+
+print(5**2)
+
+print("Bu kodlar www.suleymanguven.com.tr adresindedir")
+
